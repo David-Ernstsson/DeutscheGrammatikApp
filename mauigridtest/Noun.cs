295b90b5512ba9134038ad1bf03fedd24a466814
David Ernstsson <david.ernstsson@gmail.com>
2024-08-18 15:55:33 +0200

    Re-added files

---
+++ mauigridtest/Noun.cs
@@ -0,0 +1,17 @@
+﻿namespace mauigridtest;
+
+public class Noun
+{
+    public string Text { get; set; }
+    public string ImageSource { get; set; }
+    public string Gender { get; set; }
+
+    public static List<Noun> Nouns =
+    [
+        new Noun { Text = "Katze", Gender = "die", ImageSource = "cat.png" },
+        new Noun { Text = "Hund", Gender = "der", ImageSource = "dog.png" },
+        new Noun() { Text = "Ei", Gender = "das", ImageSource = "egg.png" },
+        new Noun() { Text = "Milch", Gender = "die", ImageSource = "milk.png" },
+        new Noun() { Text = "Apfel", Gender = "der", ImageSource = "apple.png" }
+    ];
+}
---
+++ mauigridtest/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace mauigridtest
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
