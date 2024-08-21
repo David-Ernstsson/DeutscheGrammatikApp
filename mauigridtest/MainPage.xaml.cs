6d09e2490a407d4bb215b2f5ca0d742c768784fd
David Ernstsson <david.ernstsson@gmail.com>
2024-08-21 23:47:09 +0200

    moved most things to vm

--- mauigridtest/MainPage.xaml.cs
+++ mauigridtest/MainPage.xaml.cs
@@ -1,67 +1,56 @@
 ﻿using CommunityToolkit.Maui.Markup;
 using CommunityToolkit.Maui.Views;
 using mauigridtest.Models;
-using mauigridtest.Repositories;
 using static CommunityToolkit.Maui.Markup.GridRowsColumns;
 
 namespace mauigridtest
 {
     public partial class MainPage : ContentPage
     {
-        private readonly NounRepository _nounRepository;
-        private static NounViewModel _nounViewModel = new();
-
-        private static TimeSpan DelayWhenCorrectAnswer = TimeSpan.FromMilliseconds(1500);
-        private static TimeSpan DelayWhenWrongAnswer = TimeSpan.FromSeconds(3);
-
-        private readonly Label _nounTextLabel;
+        private static NounViewModel _nounViewModel;
 
         private enum Row { Image, Text, Audio, Gender }
         private enum Column { Der, Die, Das }
 
-        private readonly IList<Button> _buttons;
-
-        private readonly Color _defaultButtonColor = Colors.LightBlue;
-        private readonly Image _image;
         private readonly MediaElement _mediaElement;
 
-        private const bool LocalResources = true;
-
-        public MainPage(NounRepository nounRepository, NounViewModel viewModel)
+        public MainPage(NounViewModel viewModel)
         {
-            viewModel.CurrentNoun = new Noun() { Text = "Loading", ImageSource = "cat.png" };
+            viewModel.CurrentNoun = new Noun { Text = "Loading", ImageSource = "cat.png" };
 
             _nounViewModel = viewModel;
-            _nounViewModel.SomeText = "loaddd";
             BindingContext = viewModel;
 
-            _nounRepository = nounRepository;
-
-            _image = new Image()
+            var image = new Image()
                 .Row(Row.Image)
-                .Column(Column.Der, Column.Das);
-            //.Bind(Image.SourceProperty, static (NounViewModel vm) => ImageSource.FromFile(vm.CurrentNoun.ImageSource));
+                .Column(Column.Der, Column.Das)
+                .Bind(Image.SourceProperty, static (NounViewModel vm) => vm.CurrentNoun.ImageSource,
+                    handlers:
+                    [
+                        (vm => vm, nameof(NounViewModel.CurrentNoun)),
+                        (vm => vm.CurrentNoun, nameof(NounViewModel.CurrentNoun.ImageSource))
+                    ]);
 
-            _image.HorizontalOptions = LayoutOptions.Center;
-            _image.VerticalOptions = LayoutOptions.Center;
+            image.HorizontalOptions = LayoutOptions.Center;
+            image.VerticalOptions = LayoutOptions.Center;
 
-            _nounTextLabel = new Label()
+            var nounTextLabel = new Label()
                 .Row(Row.Text)
                 .Column(Column.Der, Column.Das)
                 //.Bind(Label.TextProperty, nameof(viewModel.SomeText));
                 .Bind(Label.TextProperty, static (NounViewModel vm) => vm.CurrentNoun.Text,
-                    handlers: new (Func<NounViewModel, object?>, string)[]
-                    {
+                    handlers:
+                    [
                         (vm => vm, nameof(NounViewModel.CurrentNoun)),
                         (vm => vm.CurrentNoun, nameof(NounViewModel.CurrentNoun.Text))
-                    });
+                    ]);
             //.Bind(Label.TextProperty, static (NounViewModel vm) => vm.CurrentNoun.Text);
 
-            _nounTextLabel.FontSize(75);
+            nounTextLabel.FontSize(75);
 
-            _nounTextLabel.HorizontalOptions = LayoutOptions.Center;
+            nounTextLabel.HorizontalOptions = LayoutOptions.Center;
 
-            _buttons = CreateGenderButtons();
+            IList<Button> buttons = CreateGenderButtons();
 
             _mediaElement = new MediaElement()
                 .Row(Row.Audio)
@@ -86,16 +75,14 @@ namespace mauigridtest
 
                 Children =
                 {
-                    _image,
+                    image,
                     _mediaElement,
-                    _nounTextLabel,
-                    _buttons[0],
-                    _buttons[1],
-                    _buttons[2],
+                    nounTextLabel,
+                    buttons[0],
+                    buttons[1],
+                    buttons[2],
                 }
             };
-
-            Task.Run(async () => await MoveToNextNoun().ConfigureAwait(false));
         }
 
         private IList<Button> CreateGenderButtons()
@@ -114,112 +101,14 @@ namespace mauigridtest
                 .Text(gender)
                 .Row(Row.Gender)
                 .FontSize(50)
-                .BackgroundColor(_defaultButtonColor)
                 .Column(column);
 
-            button.Command = new Command(() => CheckAnswer(gender));
+            _nounViewModel.Buttons.Add(button);
+
+            button.Command = new Command(() => _nounViewModel.CheckAnswer(gender));
 
             return button;
         }
 
-        private async Task MoveToNextNoun()
-        {
-            var next = await _nounRepository.GetNext();
-            _nounViewModel.CurrentNoun = next;
-            _nounViewModel.SomeText = next.Text;
-
-            //_nounTextLabel.Text = _currentNoun.Text;
-
-            var audioPrefix = "https://deutschegrammatikblob.blob.core.windows.net/audio/";
-            var imagePrefix = "https://deutschegrammatikblob.blob.core.windows.net/images/";
-
-            //_mediaElement.Source = MediaSource.FromUri($"{audioPrefix}{_currentNoun.AudioResource}");
-            //_image.Source = ImageSource.FromUri(new Uri(imagePrefix + _currentNoun.ImageSource));
-            return;
-
-            //if (LocalResources)
-            //{
-            //    //_mediaElement.Source = MediaSource.FromResource("BigBuckBunny.mp4");
-            //    _mediaElement.Source = MediaSource.FromResource(_currentNoun.AudioResource);
-            //    _image.Source = ImageSource.FromFile(_currentNoun.ImageSource);
-            //}
-            //else
-            //{
-            //    var prefix = $"{FileSystem.Current.AppDataDirectory}\\";
-            //    _mediaElement.Source = MediaSource.FromFile($"{prefix}{_currentNoun.AudioResource}");
-            //    _image.Source = ImageSource.FromFile($"{prefix}{_currentNoun.ImageSource}");
-            //}
-        }
-
-        async void CheckAnswer(string chosenGender)
-        {
-            var correctGender = _nounViewModel.CurrentNoun.Gender;
-            try
-            {
-                _mediaElement.Play();
-            }
-            catch (Exception e)
-            {
-            }
-
-
-            HighlightGender(correctGender, Colors.Green);
-            if (chosenGender != correctGender)
-            {
-                HighlightGender(chosenGender, Colors.Red);
-            }
-
-            HideOtherButtons();
-            //_nounTextLabel.Text = $"{correctGender} {_nounViewModel.CurrentNoun.Text}";
-
-            if (chosenGender != correctGender)
-            {
-                await Task.Delay(DelayWhenWrongAnswer);
-            }
-            else
-            {
-                await Task.Delay(DelayWhenCorrectAnswer);
-            }
-
-            try
-            {
-                _mediaElement.Stop();
-            }
-            catch (Exception e)
-            {
-            }
-
-            await MoveToNextNoun();
-            ResetButtons();
-        }
-
-        private void HighlightGender(string gender, Color color)
-        {
-            var button = _buttons.Single(b => b.Text == gender);
-            button.BackgroundColor(color);
-        }
-
-        private void HideOtherButtons()
-        {
-            var buttonToHide = _buttons.Where(b => b.BackgroundColor.Equals(_defaultButtonColor));
-            foreach (var button in buttonToHide)
-            {
-                button.IsVisible = false;
-            }
-        }
-
-        private void ResetButtons()
-        {
-            foreach (var button in _buttons)
-            {
-                ResetButton(button);
-            }
-        }
-
-        private void ResetButton(Button button)
-        {
-            button.BackgroundColor(_defaultButtonColor);
-            button.IsVisible(true);
-        }
     }
 }
--- mauigridtest/NounViewModel.cs
+++ mauigridtest/NounViewModel.cs
@@ -1,24 +1,140 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System.Diagnostics;
 using mauigridtest.Models;
+using System.Runtime.CompilerServices;
+using mauigridtest.Repositories;
 
 namespace mauigridtest;
 
 public partial class NounViewModel : ObservableObject
 {
+    private readonly NounRepository _nounRepository;
+    private readonly Color _defaultButtonColor = Colors.LightBlue;
+
+    private static TimeSpan DelayWhenCorrectAnswer = TimeSpan.FromMilliseconds(1500);
+    private static TimeSpan DelayWhenWrongAnswer = TimeSpan.FromSeconds(3);
+
     [ObservableProperty]
     private Noun currentNoun;
 
-    [ObservableProperty]
-    private string someText;
-
-    partial void OnCurrentNounChanging(Noun value)
+    public NounViewModel(NounRepository nounRepository)
     {
-        Debug.WriteLine($"Name is about to change to {value}");
+        _nounRepository = nounRepository;
+
+        Task.Run(async () => await MoveToNextNoun().ConfigureAwait(false));
     }
 
-    partial void OnCurrentNounChanged(Noun value)
+    public IList<Button> Buttons { get; set; } = new List<Button>();
+
+    public async void CheckAnswer(string chosenGender)
     {
-        Debug.WriteLine($"Name has changed to {value}");
+        var correctGender = CurrentNoun.Gender;
+        try
+        {
+            //_mediaElement.Play();
+        }
+        catch (Exception e)
+        {
+        }
+
+
+        HighlightGender(correctGender, Colors.Green);
+        if (chosenGender != correctGender)
+        {
+            HighlightGender(chosenGender, Colors.Red);
+        }
+
+        HideOtherButtons();
+        //_nounTextLabel.Text = $"{correctGender} {_nounViewModel.CurrentNoun.Text}";
+
+        if (chosenGender != correctGender)
+        {
+            await Task.Delay(DelayWhenWrongAnswer);
+        }
+        else
+        {
+            await Task.Delay(DelayWhenCorrectAnswer);
+        }
+
+        //try
+        //{
+        //    _mediaElement.Stop();
+        //}
+        //catch (Exception e)
+        //{
+        //}
+
+        await MoveToNextNoun();
+        ResetButtons();
+    }
+
+    private async Task MoveToNextNoun()
+    {
+        var next = await _nounRepository.GetNext();
+        CurrentNoun = next;
+
+        //_nounViewModel.ButtonsVisible["der"] = !_nounViewModel.ButtonsVisible["der"];
+
+        //_nounTextLabel.Text = _currentNoun.Text;
+
+        var audioPrefix = "https://deutschegrammatikblob.blob.core.windows.net/audio/";
+        var imagePrefix = "https://deutschegrammatikblob.blob.core.windows.net/images/";
+
+        //_mediaElement.Source = MediaSource.FromUri($"{audioPrefix}{_currentNoun.AudioResource}");
+        //_image.Source = ImageSource.FromUri(new Uri(imagePrefix + _currentNoun.ImageSource));
+        return;
+
+        //if (LocalResources)
+        //{
+        //    //_mediaElement.Source = MediaSource.FromResource("BigBuckBunny.mp4");
+        //    _mediaElement.Source = MediaSource.FromResource(_currentNoun.AudioResource);
+        //    _image.Source = ImageSource.FromFile(_currentNoun.ImageSource);
+        //}
+        //else
+        //{
+        //    var prefix = $"{FileSystem.Current.AppDataDirectory}\\";
+        //    _mediaElement.Source = MediaSource.FromFile($"{prefix}{_currentNoun.AudioResource}");
+        //    _image.Source = ImageSource.FromFile($"{prefix}{_currentNoun.ImageSource}");
+        //}
+    }
+
+    private void HighlightGender(string gender, Color color)
+    {
+        var button = Buttons.Single(b => b.Text == gender);
+        button.BackgroundColor = color;
+    }
+
+    private void HideOtherButtons()
+    {
+        var buttonToHide = Buttons.Where(b => b.BackgroundColor.Equals(_defaultButtonColor));
+        foreach (var button in buttonToHide)
+        {
+            button.IsVisible = false;
+        }
+    }
+
+    private void ResetButtons()
+    {
+        foreach (var button in Buttons)
+        {
+            ResetButton(button);
+        }
+    }
+
+    private void ResetButton(Button button)
+    {
+        button.BackgroundColor = _defaultButtonColor;
+        button.IsVisible = true;
+    }
+
+    private static void PlaySound()
+    {
+        try
+        {
+            //_mediaElement.Play();
+        }
+        catch (Exception e)
+        {
+        }
     }
 }
